159388bc94cd8b0053c63b34562b3ec78419892d
ztachi <legendryztachi@gmail.com>
2026-04-05 20:34:51 +0800

    chore: 添加 husky 钩子以集成 commitlint 和 lint-staged
    - 新增 husky 的 pre-commit 钩子，在提交前自动运行 lint-staged
    - 新增 husky 的 commit-msg 钩子，使用 commitlint 校验提交信息格式
    - 安装 @commitlint/cli 和 @commitlint/config-conventional 依赖
    - 添加 commitlint.config.ts 配置文件，扩展常规配置并禁用长度校验

---
+++ commitlint.config.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: ztachi(legendryztachi@gmail.com)
+ * @Date: 2026-04-01 21:05:41
+ * @LastEditors: ztachi(legendryztachi@gmail.com)
+ * @LastEditTime: 2026-04-05 20:32:10
+ * @FilePath: /strawberrybear-tools/commitlint.config.ts
+ * @Description: 
+ */
+/*
+ * @Author: ztachi(legendryztachi@gmail.com)
+ * @Date: 2026-03-30 17:31:45
+ * @LastEditors: ztachi(legendryztachi@gmail.com)
+ * @LastEditTime: 2026-04-01 17:20:42
+ * @FilePath: /homepage/commitlint.config.ts
+ * @Description:
+ */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0], // 禁用 header 长度校验
+    'body-max-line-length': [0], // 禁用 body 长度校验
+  },
+}
